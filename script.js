52f47268b1ab16833341dc8700095b17dfd56e51
WindBerkker <wiatrowka@student.agh.edu.pl>
2023-11-21 13:13:19 +0100

    Phone version

---
+++ script.js
@@ -0,0 +1,52 @@
+function change_to_bal(){
+    var img = document.getElementById("image");
+
+    img.src="src/Bal.jpg";
+
+    var description = document.getElementById("opis")
+
+    description.textContent = "bal";
+    return false;
+}
+function change_to_rajd(){
+    var img = document.getElementById("image");
+
+    img.src="src/Rajd.jpg";
+    return false;
+}
+function change_to_wydzialowki(){
+    var img = document.getElementById("image");
+
+    img.src="src/wydzialowka.jpg";
+    return false;
+}
+function change_to_kawa(){
+    var img = document.getElementById("image");
+
+    img.src="src/Kawa.jpg";
+    return false;
+}
+function change_to_junior(){
+    var img = document.getElementById("image");
+
+    img.src="src/Junior.jpg";
+    return false;
+}
+function change_to_piknik(){
+    var img = document.getElementById("image");
+
+    img.src="src/Piknik.jpg";
+    return false;
+}
+function change_to_nawrzucaj(){
+    var img = document.getElementById("image");
+
+    img.src="src/Ula.jpg";
+    return false;
+}
+function change_to_bluzy(){
+    var img = document.getElementById("image");
+
+    img.src="src/Karo.jpg";
+    return false;
+}
