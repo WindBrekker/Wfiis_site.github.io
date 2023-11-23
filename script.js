b8fd5995f44daa1dad88c1427de39623d6270344
WindBerkker <wiatrowka@student.agh.edu.pl>
2023-11-23 11:58:16 +0100

    projects descriptions

--- script.js
+++ script.js
@@ -5,48 +5,83 @@ function change_to_bal(){
 
     var description = document.getElementById("opis")
 
-    description.textContent = "bal";
+    description.textContent = "Jest to coroczne wydarzenie integrujące środowisko akademickie - studentów, pracowników Uczelni oraz kadrę profesorską.  Impreza taneczna zachowana w klimacie balu, jest to ciekawy sposób na spędzenie czasu w gronie osób które spotykamy na co dzień na uczelnianych korytarzach. Co roku w wydarzeniu bierze udział około 120 osób. ";
     return false;
 }
 function change_to_rajd(){
     var img = document.getElementById("image");
 
     img.src="src/Rajd.jpg";
+
+    var description = document.getElementById("opis")
+
+    description.textContent = "Weekendowy wyjazd poza Kraków - zwykle w góry. Czas pełen sportu, integracji i świeżego powietrza. Wyjścia w góry, zwiedzanie ciekawych miejsc, poznawanie innych studentów - to tylko część  tego co dzieje się na Rajdach, warto przekonać się samemu :)";
+
     return false;
 }
 function change_to_wydzialowki(){
     var img = document.getElementById("image");
 
     img.src="src/wydzialowka.jpg";
+
+        var description = document.getElementById("opis")
+
+    description.textContent = "Imprezy organizowane w klubie Studio we współpracy z WRSS-ami innych wydziałów. Każda z nich ma motyw przewodni, a o dobrą zabawę dbają rezydenci klubu Studio - DJ Namteh i DJ Antybiotyk.";
+
     return false;
 }
 function change_to_kawa(){
     var img = document.getElementById("image");
 
     img.src="src/Kawa.jpg";
+
+        var description = document.getElementById("opis")
+
+    description.textContent = "Comiesięczne wydarzenie organizowane w czwartki w naszym biurze (p. 235 w budynku D-10). Za każdym razem zapraszamy studentów oraz pracowników naszego wydziału na słodki poczęstunek i kubek ciepłego napoju, aby porozmawiać o bieżących sprawach i aby mogli poznać bliżej członków naszego WRSSu.";
+
     return false;
 }
 function change_to_junior(){
     var img = document.getElementById("image");
 
     img.src="src/Junior.jpg";
+
+        var description = document.getElementById("opis")
+
+    description.textContent = "Inicjatywa mająca na celu zapoznanie młodszych stażem studentów z organizacją jaką jest WRSS. Daje możliwość współpracy z samorządem, zobaczenia mechanizmu funkcjonowania od wewnątrz oraz  większego zaangażowania w życie wydziału. W roku akademickim 2022/2023 odbyła się pierwsza edycja, po której kilkoro uczestników WRSS Junior dołączyło w wyborach uzupełniających do WRSS WFiIS.    ";
+
     return false;
 }
 function change_to_piknik(){
     var img = document.getElementById("image");
 
     img.src="src/Piknik.jpg";
+
+        var description = document.getElementById("opis")
+
+    description.textContent = "Wydarzenie, które odbywa się w okresie wiosenno-letnim na Miasteczku Studenckim AGH. Zapraszamy wtedy studentów i pracowników naszego wydziału na grilla, gry zespołowe i integrację w przyjaznym otoczeniu. Jedyna i niepowtarzalna okazja, aby zagrać ze swoim prowadzącym we flanki ;)";
+
     return false;
 }
 function change_to_nawrzucaj(){
     var img = document.getElementById("image");
 
     img.src="src/Ula.jpg";
+
+        var description = document.getElementById("opis")
+
+    description.textContent = "Akcja w ramach wydarzenia “AGH Święta Dzieciom”, w czasie której Dziekani naszego wydziału wraz z członkami WRSS WFiIS zbierają pieniądze w szczytnym celu. W 2023 roku zbiórka odbędzie się dla Stowarzyszenia na Rzecz Dzieci z Chorobą Nowotworową Wyspy Szczęśliwe.";
+
     return false;
 }
 function change_to_bluzy(){
     var img = document.getElementById("image");
 
     img.src="src/Karo.jpg";
+
+        var description = document.getElementById("opis")
+
+    description.textContent = "Co roku wybieramy najlepsze jakościowo bluzy, aby ogrzewały studentów w chłodniejsze dni. Jest to również świetna okazja, aby umożliwić studentom pokazywanie dumy ze swojego wydziału, poprzez prezentowanie nazwy wydziału na swoim ubraniu.     ";
+
     return false;
 }
