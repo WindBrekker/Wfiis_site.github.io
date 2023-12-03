7edfaed8b78af4d4c198e576e914e25cca16631a
WindBerkker <wiatrowka@student.agh.edu.pl>
2023-12-03 19:09:03 +0100

    New images

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function change_to_bal(){
 
     var description = document.getElementById("opis")
 
-    description.textContent = "Jest to coroczne wydarzenie integrujące środowisko akademickie - studentów, pracowników Uczelni oraz kadrę profesorską.  Impreza taneczna zachowana w klimacie balu, jest to ciekawy sposób na spędzenie czasu w gronie osób które spotykamy na co dzień na uczelnianych korytarzach. Co roku w wydarzeniu bierze udział około 120 osób. ";
+    description.textContent = "Bal Fizyka to coroczne wydarzenie integrujące środowisko akademickie - studentów, pracowników Uczelni oraz kadrę profesorską.  Impreza taneczna zachowana w klimacie balu, jest to ciekawy sposób na spędzenie czasu w gronie osób które spotykamy na co dzień na uczelnianych korytarzach. Co roku w wydarzeniu bierze udział około 120 osób. ";
     return false;
 }
 function change_to_rajd(){
@@ -15,7 +15,7 @@ function change_to_rajd(){
 
     var description = document.getElementById("opis")
 
-    description.textContent = "Weekendowy wyjazd poza Kraków - zwykle w góry. Czas pełen sportu, integracji i świeżego powietrza. Wyjścia w góry, zwiedzanie ciekawych miejsc, poznawanie innych studentów - to tylko część  tego co dzieje się na Rajdach, warto przekonać się samemu :)";
+    description.textContent = "Rajd to weekendowy wyjazd poza Kraków - zwykle w góry. Czas pełen sportu, integracji i świeżego powietrza. Wyjścia w góry, zwiedzanie ciekawych miejsc, poznawanie innych studentów - to tylko część  tego co dzieje się na Rajdach, warto przekonać się samemu :)";
 
     return false;
 }
@@ -26,18 +26,18 @@ function change_to_wydzialowki(){
 
         var description = document.getElementById("opis")
 
-    description.textContent = "Imprezy organizowane w klubie Studio we współpracy z WRSS-ami innych wydziałów. Każda z nich ma motyw przewodni, a o dobrą zabawę dbają rezydenci klubu Studio - DJ Namteh i DJ Antybiotyk.";
+    description.textContent = "Wydziałówki to imprezy organizowane w klubie Studio we współpracy z WRSS-ami innych wydziałów. Każda z nich ma motyw przewodni, a o dobrą zabawę dbają rezydenci klubu Studio - DJ Namteh i DJ Antybiotyk.";
 
     return false;
 }
 function change_to_kawa(){
     var img = document.getElementById("image");
 
-    img.src="src/Kawa.jpg";
+    img.src="src/Kawa.png";
 
         var description = document.getElementById("opis")
 
-    description.textContent = "Comiesięczne wydarzenie organizowane w czwartki w naszym biurze (p. 235 w budynku D-10). Za każdym razem zapraszamy studentów oraz pracowników naszego wydziału na słodki poczęstunek i kubek ciepłego napoju, aby porozmawiać o bieżących sprawach i aby mogli poznać bliżej członków naszego WRSSu.";
+    description.textContent = "Kawa z WRSSem to comiesięczne wydarzenie organizowane w czwartki w naszym biurze (p. 235 w budynku D-10). Za każdym razem zapraszamy studentów oraz pracowników naszego wydziału na słodki poczęstunek i kubek ciepłego napoju, aby porozmawiać o bieżących sprawach i aby mogli poznać bliżej członków naszego WRSSu.";
 
     return false;
 }
@@ -48,7 +48,7 @@ function change_to_junior(){
 
         var description = document.getElementById("opis")
 
-    description.textContent = "Inicjatywa mająca na celu zapoznanie młodszych stażem studentów z organizacją jaką jest WRSS. Daje możliwość współpracy z samorządem, zobaczenia mechanizmu funkcjonowania od wewnątrz oraz  większego zaangażowania w życie wydziału. W roku akademickim 2022/2023 odbyła się pierwsza edycja, po której kilkoro uczestników WRSS Junior dołączyło w wyborach uzupełniających do WRSS WFiIS.    ";
+    description.textContent = "WRSS JUNIOR to inicjatywa mająca na celu zapoznanie młodszych stażem studentów z organizacją jaką jest WRSS. Daje możliwość współpracy z samorządem, zobaczenia mechanizmu funkcjonowania od wewnątrz oraz  większego zaangażowania w życie wydziału. W roku akademickim 2022/2023 odbyła się pierwsza edycja, po której kilkoro uczestników WRSS Junior dołączyło w wyborach uzupełniających do WRSS WFiIS.    ";
 
     return false;
 }
@@ -59,25 +59,25 @@ function change_to_piknik(){
 
         var description = document.getElementById("opis")
 
-    description.textContent = "Wydarzenie, które odbywa się w okresie wiosenno-letnim na Miasteczku Studenckim AGH. Zapraszamy wtedy studentów i pracowników naszego wydziału na grilla, gry zespołowe i integrację w przyjaznym otoczeniu. Jedyna i niepowtarzalna okazja, aby zagrać ze swoim prowadzącym we flanki ;)";
+    description.textContent = "Piknik z WRSSem to wydarzenie, które odbywa się w okresie wiosenno-letnim na Miasteczku Studenckim AGH. Zapraszamy wtedy studentów i pracowników naszego wydziału na grilla, gry zespołowe i integrację w przyjaznym otoczeniu. Jedyna i niepowtarzalna okazja, aby zagrać ze swoim prowadzącym we flanki ;)";
 
     return false;
 }
 function change_to_nawrzucaj(){
     var img = document.getElementById("image");
 
-    img.src="src/Ula.jpg";
+    img.src="src/nawrzucaj.png";
 
         var description = document.getElementById("opis")
 
-    description.textContent = "Akcja w ramach wydarzenia “AGH Święta Dzieciom”, w czasie której Dziekani naszego wydziału wraz z członkami WRSS WFiIS zbierają pieniądze w szczytnym celu. W 2023 roku zbiórka odbędzie się dla Stowarzyszenia na Rzecz Dzieci z Chorobą Nowotworową Wyspy Szczęśliwe.";
+    description.textContent = "naWRZUCAJ dziekanowi to akcja w ramach wydarzenia “AGH Święta Dzieciom”, w czasie której Dziekani naszego wydziału wraz z członkami WRSS WFiIS zbierają pieniądze w szczytnym celu. W 2023 roku zbiórka odbędzie się dla Stowarzyszenia na Rzecz Dzieci z Chorobą Nowotworową Wyspy Szczęśliwe.";
 
     return false;
 }
 function change_to_bluzy(){
     var img = document.getElementById("image");
 
-    img.src="src/Karo.jpg";
+    img.src="src/Bluzy.png";
 
         var description = document.getElementById("opis")
 
